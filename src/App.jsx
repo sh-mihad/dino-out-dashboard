57fa7ba3465f69278d71d2924194da01c0671ea2
Sabbir <sh.mihad.cse@gmail.com>
2025-05-17 19:18:43 +0600

    added functionality and define ordersState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react"
 import LeftBar from "./components/LeftBar"
 import Navbar from "./components/Navbar"
 import RightSideBar from "./components/RightSideBar"
 
 
 function App() {
+  const [orders,setOrders] = useState([])
+
+  const handleAddOrder = (item)=>{
+    setOrders([...orders,{...item,id:order.length+1}])
+  }
+  const handleChangeStatus = (id)=>{
+    const nextOrders = orders.map(item=>{
+      if(item.id === id){
+        return {...item,isDelivered:true}
+      }else{
+        return item
+      }
+    })
+    setOrders(nextOrders)
+  }
+  const handleDeleteOrder = (id)=>{
+    const nextOrders = orders.filter(item=>item.id !== id)
+    setOrders(nextOrders)
+  }
 
   return (
   <div className='container mx-auto px-4 h-screen flex flex-col'>
